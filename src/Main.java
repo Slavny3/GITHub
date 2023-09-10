a3988ee363c9f9b96b0f068dd98f1f003f21dcc1
Vyacheslav <turslava03@gmail.com>
2023-09-10 15:53:35 +0300

    1.2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("GIT REPOSITORY");
         System.out.println("Easy peasy Lemon Squeezy");
+        System.out.println("Hello World");
     }
 }
